503913102aaf2aaf0c66c0932f388aab45540fd2
Alejandro Corsino <alecorsino@gmail.com>
2018-03-09 15:30:50 -0800

    Model Transform View and Perpective

--- demo/demo.js
+++ demo/demo.js
@@ -2,50 +2,76 @@ document.addEventListener("DOMContentLoaded",
   function (){
 
   console.log('[INIT]')
-  let c = Flick.createCanvas('Mycanvas', 500,500);
+  let c = Flick.createCanvas('Mycanvas', 1000,600);
   document.body.appendChild(c);
   let screen = new Flick.WebGL2(c);
   
   let programCustom = new Flick.ShaderProgram(vsCode, fsCode, screen.gl);
+  let programDefault = new Flick.ShaderProgram(Flick.DefaultVertexShader, Flick.DefaultFragmentShader, screen.gl);
   console.log('[FLICK - WebGL2]',programCustom)
 
   // window.wgl = screen;
 
   let m1 = new Flick.Model(screen.gl);
-  m1.setProgram(programCustom);
+  m1.setProgram(programDefault);
   let cubeMesh = Flick.WavefrontParser.parse(cube);
-  
+  let planeMesh = Flick.WavefrontParser.parse(plane);
   m1.verticesData = cubeMesh.vertices;
   m1.indicesData = cubeMesh.indices;
   m1.setupBuffers();
 
+  m1.transform([0,0,0], 0, [0,0,0], [1,1,1]);
+  
   let m2 = new Flick.Model(screen.gl);
   
-  let programDefault = new Flick.ShaderProgram(Flick.DefaultVertexShader, Flick.DefaultFragmentShader, screen.gl);
-  m2.setProgram(programDefault);
+  m2.setProgram(programCustom);
   
-  m2.verticesData =
-  [
-    0,  0.3, 0.0,  // top right
-    0.7, -0.5, 0.0,  // bottom right
-    -0.5, -0.5, 0.0,  // bottom let
-    -0.5,  0.5, 0.0 
-  ];
+  m2.verticesData = planeMesh.vertices;
+  m2.indicesData = planeMesh.indices;
+  m2.transform([0,0,0], 0, [0,0,0], [4,1,4]);
   
-  m2.indicesData = 
-  [
-    0, 1, 3,   // first triangle
-    1, 2, 3    // second triangle
-  ];
   m2.setupBuffers();
   
   let stage = new Flick.Stage();
-  stage.addModel(m1);
   stage.addModel(m2);
+  stage.addModel(m1);
   // screen.setSize(window.innerWidth, window.innerHeight);
   screen.init();
   screen.render(stage)
 
+  c.addEventListener("mousemove", function( event ) {
+    // store a ref. on the dragged elem
+    let x = event.screenX;
+    let y = event.screenY;
+    // console.log('[MOUSE]', x,':',y);
+    // m1.transform([0,0,0], 0, [x/100,-y/100,0], [1,1,1]);
+    // screen.lookAt(Math.sin(x/100),10, Math.cos(y/100));
+    // screen.render(stage)
+}, false);
+
+let x = 0;
+let r = 10; 
+let prevFrame = performance.now();
+const draw = (timestamp)=>{
+   // console.log('[INTERVAL]',x);
+  if((timestamp - prevFrame) >16){
+    prevFrame = timestamp;
+    x+=.01;
+   screen.lookAt(r * Math.cos(x),r, r * Math.sin(x));
+  //  screen.lookAt(r,r * Math.sin(x), r * Math.cos(x));
+   screen.render(stage)
+  }
+  requestAnimationFrame(draw)
+   
+}
+requestAnimationFrame(draw)
+
+//   document.addEventListener("mousemove", function( event ) {
+//     // store a ref. on the dragged elem
+//     dragged = event;
+//     console.log('[MOUSE]', dragged);
+//     // m1.transform([0,0,0], 0, [-7,-5,0], [1,1,1]);
+// }, false);
 
 
 
--- lib/webgl2/shaders/DefaultVShader.vs
+++ lib/webgl2/shaders/DefaultVShader.vs
@@ -3,11 +3,15 @@
 // an attribute is an input (in) to a vertex shader.
 // It will receive data from a buffer
 layout (location = 0) in vec3 a_position;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
  
 // all shaders have a main function
 void main() {
  
   // gl_Position is a special variable a vertex shader
   // is responsible for setting
-  gl_Position = vec4(a_position, 1.0);
+  gl_Position = projection * view * model * vec4(a_position, 1.0);
 }
